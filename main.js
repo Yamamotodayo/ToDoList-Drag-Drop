71f886c7f7aee90ae6eebfcc404be64b22df8787
Yamamotodayo <ocsyamamotoyuto@gmail.com>
2022-07-03 23:55:22 +0900

    html css  追加

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const taskValue = document.getElementsByClassName('task-value')[0]
+const taskSubmit = document.getElementsByClassName('task-submit')[0]
+const taskList = document.getElementsByClassName('task-list')[0]
+
+
+taskSubmit.addEventListener('click')
+
