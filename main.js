874a194aab825c61d1c1e62fea710bae1a085217
yamamotodayo <ocsyamamotoyuto@gmail.com>
2022-07-04 15:17:33 +0900

    classnameをIDに変更

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 'use strict'
 
-const taskValue = document.getElementsByClassName('task-value')[0]
-const taskSubmit = document.getElementsByClassName('task-submit')[0]
-const taskList = document.getElementsByClassName('task-list')[0]
+// const taskValue = document.getElementsByClassName('task-value')[0]
+// const taskSubmit = document.getElementsByClassName('task-submit')[0]
+// const taskList = document.getElementsByClassName('task-list')[0]
 
+const tasValue = document.getElementById("task-Value")
+const taskSubmit = document.getElementById("task-submit")
+const taskList = document.getElementById("task-list")
 
-taskSubmit.addEventListener('click')
-
+taskSubmit2.addEventListener('click')
