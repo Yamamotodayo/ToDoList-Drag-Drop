f79944a04c943758f4a0243b836b1ecd8bad7203
Yamamotodayo <ocsyamamotoyuto@gmail.com>
2022-07-05 23:36:40 +0900

    テキストボックスの文字を保存してリストに追加する機能追加

--- main.js
+++ main.js
@@ -4,18 +4,37 @@
 // const taskSubmit = document.getElementsByClassName('task-submit')[0]
 // const taskList = document.getElementsByClassName('task-list')[0]
 
-const tasValue = document.getElementById("task-Value")
+const taskValue = document.getElementById("task-value")
 const taskSubmit = document.getElementById("task-submit")
 const taskList = document.getElementById("task-list")
 
-taskSubmit2.addEventListener('click')
-/**
- * 
- * @param {*} hoge 
- * @returns 
- */
-function hoge (hoge) {
-    return hoge + "hoge"
+// let task = document.getElementById("task-Value").value
+
+
+taskSubmit.addEventListener('click', addTask) // addTask() とするとボタンを押していないのに押した時の動きになってる
+
+function addTask() {
+    let taskListItem = document.createElement('li')
+        taskListItem.classList.add("task-list-item")
+        // taskListItem.id = "item-1"
+
+    let value =  document.getElementById("task-value").value
+
+    taskList.appendChild(taskListItem)
+    taskListItem.appendChild(document.createTextNode(value))
+
+    taskValue.value = ""
 }
 
-hoge()
+
+
+// /**
+//  * 
+//  * @param {*} hoge 
+//  * @returns 
+//  */
+// function hoge (hoge) {
+//     return hoge + "hoge"
+// }
+
+// hoge()
