38164c31f92b0c3ca7e1cc8a28288258f8ea410a
Yamamotodayo <ocsyamamotoyuto@gmail.com>
2022-07-09 21:58:49 +0900

    ローカルストレージ修正

--- backup.js
+++ backup.js
@@ -13,63 +13,68 @@ const todos = JSON.parse(localStorage.getItem("todos"))
 if(todos) {
     todos.forEach((todo) => {
         addTask(todo)
+        console.log(todo);
     })
 }
 
-taskSubmit.addEventListener('click', addTask) // addTask() とするとボタンを押していないのに押した時の動きになってる
+taskSubmit.addEventListener('click', function(event) {  // addTask() とするとボタンを押していないのに押した時の動きになってる
+    event.preventDefault()
+    addTask()
+})
+
+
 
 function addTask(todo) {
 
     let value = taskValue.value // テキストボックスの値を取得
 
+    if(todo) {
+        value = todo.Text
+        console.log(value);
+    }
     
+    if(value) {
 
+        const li = document.createElement('li') // li要素生成
 
-    let li = document.createElement('li') // li要素生成
+        // li.innerText = value
         li.classList.add("list-item") // li要素にクラス名付与
 
-        li.setAttribute('draggable', true)
-
+        // li.setAttribute('draggable', true)
 
     let p = document.createElement('p')
+        p.appendChild(document.createTextNode(value))
 
     let deleteBtn = document.createElement('button') // button要素生成
         deleteBtn.classList.add("list-item-deleteBtn") // button要素にクラス名付与
         deleteBtn.append("削除")
-        // li.id = "item-1"
-
-    // let value = taskValue.value // テキストボックスの値を取得
-        
-
-    ul.appendChild(li) // ul要素の子要素にliを追加
-    p.appendChild(document.createTextNode(value))
-
-    li.appendChild(p) // テキストボックスの値をテキストノードに変換してliの子要素に追加
-    li.appendChild(deleteBtn) // li要素の子要素にbuttonを追加
-
-    if(todo) {
-        value = todo.text
-    }
-
-    taskValue.value = "" // テキストボックスの値を元に戻す
 
     // デリートボタン
-    deleteBtn.addEventListener('click', () => {
+    deleteBtn.addEventListener('click', (e) => {
+        e.preventDefault()
         let deleteTask = deleteBtn.parentNode // deleteBtnの親要素liを取得
             deleteTask.remove() // 取得したliを削除
             saveDate()
     })
+        
 
+    ul.appendChild(li) // ul要素の子要素にliを追加
+    li.appendChild(deleteBtn) // li要素の子要素にbuttonを追加
+    li.appendChild(p)
+    
+    taskValue.value = "" // テキストボックスの値を元に戻す
     saveDate()
+    }
 
+   
     // // ローカルストレージ にセーブ
     function saveDate() {
-        const lists = document.querySelectorAll("li")
+        const lists = document.querySelectorAll("p")
         const todos = []
         console.log(lists);
 
-            lists.forEach((li) => {
-                todos.push({Text:li.innerHTML}) // 修正する
+            lists.forEach((p) => {
+                todos.push({Text:p.innerText}) // 修正する
                 console.log(todos);
             })
 
